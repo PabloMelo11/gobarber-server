97412f76b3749624aff54f5ca09ecd3cae9c9025
PaabloMelo <pablo.melo@tangotech.com.br>
2020-10-30 19:03:47 -0300

    feat: relations in models

--- src/database/migrations/1604085245228-CreateAppointments.ts
+++ src/database/migrations/1604085245228-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1604085245228
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
