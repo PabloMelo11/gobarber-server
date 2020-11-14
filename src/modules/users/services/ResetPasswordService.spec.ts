0395004d4efceb0504534d08cf73465c0da0724a
PaabloMelo <pablo.melo@tangotech.com.br>
2020-11-14 17:01:19 -0300

    test: not be able reset the password non existing user

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -46,10 +46,23 @@ describe('resetPassword', () => {
     expect(updatedUser?.password).toBe('123123');
   });
 
-  it('should not be ablr to reset the password with non-existing token', async () => {
+  it('should not be able to reset the password with non-existing token', async () => {
     await expect(
       resetPassword.execute({
         token: 'non-existing-token',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset the password with non-existing user', async () => {
+    const { token } = await fakeUserTokensRepository.generate(
+      'non-existing user',
+    );
+
+    await expect(
+      resetPassword.execute({
+        token,
         password: '123456',
       }),
     ).rejects.toBeInstanceOf(AppError);
