aa21a529ab1cfc628b9c6c6704ea4b4c59a2539a
PaabloMelo <pablo.melo@tangotech.com.br>
2020-12-17 18:03:58 -0300

    feat: invalidation cache in list all providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -30,6 +30,8 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
+      console.log('A query foi feita no banco de dados');
+
       await this.cacheProvider.save(`providers-list:${user_id}`, users);
     }
 
