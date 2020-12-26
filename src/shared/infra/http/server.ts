a603019b631b4560ae647ebe0a0f25fb488a61fa
PaabloMelo <pablo.melo@tangotech.com.br>
2020-12-26 15:38:06 -0300

    feat: configuration rate limiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,16 +4,18 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import { errors } from 'celebrate';
 import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from '@config/upload';
-
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
