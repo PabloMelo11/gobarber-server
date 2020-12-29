d3808f12a0bd3c7385d95c01c1a37f8038e7059d
PaabloMelo <pablo.melo@tangotech.com.br>
2020-12-28 23:53:04 -0300

    chore: serializer providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
-import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
@@ -30,7 +30,10 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
